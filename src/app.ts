067d5b8251595be3bb872a00a4bf0c96ecc021fa
alialaba <alialaba079@gmail.com>
2023-07-08 16:02:26 -0400

    ch:implement cors and rate limit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, {Express, Request, Response} from "express";
-// import cors from;
+import cors from "cors";
+import { rateLimit } from 'express-rate-limit';
+
 
 import connectDB from "./config/ds";
 import authRoute from "./Routes/Auth";
@@ -17,8 +19,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// app.use(cors());
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
+
+
+app.use(cors());
+app.use(limiter);
 //Routes 
 app.use("/auth", authRoute);
 app.use("/user", urlRoute);
